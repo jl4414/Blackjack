3d2ee1056b15c1174e1a139fcf536d7587c90ce5
Jackson Asaro <jacksonasaro@Jacksons-MacBook-Pro.local>
2020-10-12 13:28:58 +0100

    Add functionality for hit/stand option. Still need to solve asynch issues.

--- app.js
+++ app.js
@@ -7,13 +7,47 @@ var bank = 100;
 console.log("Welcome to Blackjack! You are starting with $" + bank);
 
 //Define scheme for data entries
-var betSchema = {
+const betSchema = {
   properties: {
     bet: {
       type: 'integer',
       maximum: bank,
-      message: "The bet must be an integer less than or equal to the bank balance",
+      minimum: 1,
+      message: "The bet must be an integer less than or equal to the bank balance and greater than one",
     },
+  }
+};
+
+const hitStandSchema = {
+  properties: {
+    decision: {
+      type: 'integer',
+      minimum: 1,
+      maxmimum: 2,
+      message: "Select by choosing the number of your choice",
+    }
+  }
+};
+
+const hitStandDoubleSchema = {
+  properties: {
+    decision: {
+      type: 'integer',
+      minimum: 1,
+      maxmimum: 3,
+      message: "Select by choosing the number of your choice",
+    }
+  }
+};
+
+const hitStandDoubleSplitSchema = {
+  properties: {
+    decision: {
+      type: 'integer',
+      minimum: 1,
+      maxmimum: 4,
+      message: "Select by choosing the number of your choice",
+    }
   }
 };
 
@@ -25,14 +59,53 @@ prompt.get(betSchema, function(err, result){
   }
   else{
     var bet = result.bet;
+    bank = bank - bet;
 
     //Deal
-    playerHand = Array.from(deckOCards.randomizedDeck()).slice(0,2);
-    dealerHand = Array.from(deckOCards.randomizedDeck()).slice(0,2);
+    var playerHand = Array.from(deckOCards.randomizedDeck()).slice(0,2);
+    var dealerHand = Array.from(deckOCards.randomizedDeck()).slice(0,2);
 
     console.log("The dealer is showing a " + dealerHand[0].name + " " + dealerHand[0].type);
     console.log("You have a " + playerHand[0].name + " " + playerHand[0].type + "  and a " + playerHand[1].name + " " + playerHand[1].type);
 
-  }
+    //Intiate control flow based on the player's cards and bank
+    //Tree for if the the player has a pair
+    if (playerHand[0].number == playerHand[1].number) {
+      if (bet * 2 <= bank) {
+        console.log("You can either hit (1), stand (2), double (3) or split (4)");
+        prompt.start();
+        prompt.get(hitStandDoubleSplitSchema, function(err, result){
+          if (err) {
+            console.log(err);
+          }
+          else {
 
+          }
+        });
+      }
+      else {
+        console.log("You can either hit (1) or stand (2)");
+        prompt.start();
+        prompt.get(hitStandSchema, function(err, result){
+          if (err) {
+            console.log(err);
+          }
+          else {
+
+          }
+        });
+      }
+    }
+    //Tree for when the player does not have a double
+    else {
+      if (bet * 2 <= bank) {
+        console.log("You can either hit (1), stand (2) or double (3)");
+
+      }
+      else {
+        var x = blackjack.recursiveHitOrStand(playerHand);
+        console.log(x);
+      }
+    }
+  }
 });
--- blackjackModule.js
+++ blackjackModule.js
@@ -1,8 +1,116 @@
-exports.validateBet = function(bankBalance, bet) {
-  if (bet > balance) {
-    return false;
-  }
-  else {
-    return true;
+//jshint esversion:6
+const deckOCards = require('deck-o-cards');
+const prompt = require('prompt');
+
+
+//Schemas
+const hitStandSchema = {
+  properties: {
+    decision: {
+      type: 'integer',
+      minimum: 1,
+      maxmimum: 2,
+      message: "Select by choosing the number of your choice",
+    }
   }
 };
+
+const hitStandDoubleSchema = {
+  properties: {
+    decision: {
+      type: 'integer',
+      minimum: 1,
+      maxmimum: 3,
+      message: "Select by choosing the number of your choice",
+    }
+  }
+};
+
+const hitStandDoubleSplitSchema = {
+  properties: {
+    decision: {
+      type: 'integer',
+      minimum: 1,
+      maxmimum: 4,
+      message: "Select by choosing the number of your choice",
+    }
+  }
+};
+
+//hit
+newCard = function() {
+  card = Array.from(deckOCards.randomizedDeck()).slice(0,1);
+  return card;
+};
+
+//
+recursiveHitOrStand = function (hand) {
+  console.log("You can either hit (1), or stand (2)");
+  prompt.start();
+  prompt.get(hitStandSchema, function(err, result) {
+    switch (result.decision) {
+      case(1):
+        hand = hand.concat(newCard());
+        console.log("You have been dealt a " + hand[hand.length - 1].name + " " + hand[hand.length - 1].type);
+        printHand(hand);
+        if (sumHand(hand) < 22) {
+          return recursiveHitOrStand(hand);
+        }
+        else {
+          console.log("You busted!");
+          return sumHand(hand);
+        }
+        break;
+      case(2):
+        return sumHand(hand);
+    }
+  });
+};
+
+
+//sum hand
+sumHand = function(hand) {
+  var sum1 = 0;
+  var sum11 = 0;
+  hand.forEach(function(card){
+    if (card.number > 1) {
+      sum1 += card.number;
+      sum11 += card.number;
+    }
+    else {
+      sum1 += 1;
+      sum11 += 11;
+    }
+  });
+  if (sum11 < 22) {
+    return sum11;
+  }
+  else {
+    return sum1;
+  }
+};
+
+//check for bust
+checkForBust = function(hand) {
+  sum = sumHand(hand);
+  if (sum > 22) {
+    return true;
+  }
+  else {
+    return false;
+  }
+};
+
+//print hand
+printHand = function(hand) {
+  console.log("Your hand is: ");
+  hand.forEach(function(card){
+    console.log(card.name + " " + card.type);
+  });
+};
+
+exports.newCard = newCard;
+exports.recursiveHitOrStand = recursiveHitOrStand;
+exports.sumHand = sumHand;
+exports.checkForBust = checkForBust;
+exports.printhand = printHand;
